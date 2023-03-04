b2c4d2b15c980eb3fb9bd1180363465188c3dfee
Untouch <bahodir5563325@gmail.com>
2023-03-04 20:43:11 +0500

    add 1mainSolution.go

---
+++ 1mainSolution.go
@@ -0,0 +1,18 @@
+package main
+
+// first exercise
+//func main() {
+//
+//	for i := 0; i < 50*2; i += 2 {
+//		fmt.Println(i)
+//	}
+//}
+
+// second exesice
+//func main() {
+//	b := 0
+//	for i := 0; i < 99; i += 3 {
+//		fmt.Println(b)
+//		b += 3
+//	}
+//}
