2b9d84ecd0c984db5aee71e627e7837694393509
Untouch <bahodir5563325@gmail.com>
2023-03-05 16:50:28 +0500

    add 1mainSolution.go solved the first three tasks

--- 1mainSolution.go
+++ 1mainSolution.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // first exercise
 //func main() {
 //
@@ -8,7 +10,7 @@ package main
 //	}
 //}
 
-// second exesice
+// second exercise
 //func main() {
 //	b := 0
 //	for i := 0; i < 99; i += 3 {
@@ -16,3 +18,44 @@ package main
 //		b += 3
 //	}
 //}
+
+//// third exercise
+//func main() {
+//	even := 0
+//	odd := 1
+//	for i := 0; i < 99; i += 2 {
+//		even += 2
+//		odd += 2
+//		s := "ok!"
+//		gg := "neok!"
+//		fmt.Println(even, s)
+//		fmt.Println(odd, gg)
+//	}
+//
+//}
+func main() {
+
+	all := []int{}
+	five := []int{}
+	three := []int{}
+	t, f, a := 0, 0, 0
+
+	for i := 0; i < 99; i++ {
+		t += 3
+		f += 5
+		all = append(all, i)
+		three = append(three, t)
+		five = append(five, f)
+		return
+	}
+	for a, all = range all {
+		switch all {
+		case three:
+			fmt.Printf("%v три.\n", a)
+		default:
+			fmt.Printf("%v ok.\n", a)
+
+		}
+	}
+
+}
