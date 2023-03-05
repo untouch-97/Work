9ecdb159d7daf4110b51a2416757d1052b19888f
Untouch <bahodir5563325@gmail.com>
2023-03-06 00:15:37 +0500

    add 1mainSolution.goÐ all tasks completed

--- 1mainSolution.go
+++ 1mainSolution.go
@@ -1,8 +1,6 @@
 package main
 
-import "fmt"
-
-// first exercise
+// first task
 //func main() {
 //
 //	for i := 0; i < 50*2; i += 2 {
@@ -10,7 +8,7 @@ import "fmt"
 //	}
 //}
 
-// second exercise
+// second task
 //func main() {
 //	b := 0
 //	for i := 0; i < 99; i += 3 {
@@ -19,44 +17,72 @@ import "fmt"
 //	}
 //}
 
-//// third exercise
+// // third task
+//
+//	func main() {
+//		even := 0
+//		odd := 1
+//		for i := 0; i < 99; i += 2 {
+//			even += 2
+//			odd += 2
+//			s := "ok!"
+//			gg := "neok!"
+//			fmt.Println(even, s)
+//			fmt.Println(odd, gg)
+//		}
+//
+// }
+
+//					fourth task
 //func main() {
-//	even := 0
-//	odd := 1
-//	for i := 0; i < 99; i += 2 {
-//		even += 2
-//		odd += 2
-//		s := "ok!"
-//		gg := "neok!"
-//		fmt.Println(even, s)
-//		fmt.Println(odd, gg)
+//	for i := 0; i <= 99; i++ {
+//		if i%3 == 0 && i%5 == 0 {
+//			fmt.Printf("%v 3 and 5 \n", i)
+//		} else if i%3 == 0 {
+//			fmt.Printf("%v 3 \n", i)
+//		} else if i%5 == 0 {
+//			fmt.Printf("%v 5 \n", i)
+//		} else {
+//			fmt.Printf("%v Баха крассваучик еЖЖи!\n", i)
+//		}
+//
+//	}
+//}
+//func main() {
+//	for i := 0; i <= 99; i++ {
+//		switch {
+//		case i%3 == 0 && i%5 == 0:
+//			fmt.Printf("%v 3 and 5 \n", i)
+//		case i%3 == 0:
+//			fmt.Printf("%v 3 \n", i)
+//		case i%5 == 0:
+//			fmt.Printf("%v 5 \n", i)
+//		default:
+//			fmt.Printf("%v Баха крассаучик еЖЖи!\n", i)
+//
+//		}
+//	}
+//}
+
+// fifth task
+
+//func boom() error {
+//	return errors.New("age entered incorrectly")
+//}
+//func main() {
+//	err := boom()
+//	var age int
+//	fmt.Println("Enter your age:")
+//	fmt.Scanln(&age)
+//	switch {
+//	case age >= 0 && age < 18:
+//		fmt.Println("молодой огурчик!")
+//	case age >= 0 && age < 60:
+//		fmt.Println("уже не такой молодой огурчик")
+//	case age >= 60:
+//		fmt.Println("пажилая каструля")
+//	case err != nil:
+//		fmt.Println(err)
 //	}
 //
 //}
-func main() {
-
-	all := []int{}
-	five := []int{}
-	three := []int{}
-	t, f, a := 0, 0, 0
-
-	for i := 0; i < 99; i++ {
-		a += 0
-		t += 3
-		f += 5
-		all = append(all, i)
-		three = append(three, t)
-		five = append(five, f)
-		return
-	}
-	for a, all = range all {
-		switch all {
-		case three:
-			fmt.Printf("%v три.\n", a)
-		default:
-			fmt.Printf("%v ok.\n", a)
-
-		}
-	}
-
-}
