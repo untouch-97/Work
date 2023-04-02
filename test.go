50e4cdc67444b8ec9aec20477a28b1e595437c8b
untouch <Bahodir5563325@gmail.com>
2023-04-02 12:44:37 +0500

    add new question test.go

--- test.go
+++ test.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
+//	type Vertex struct {
+//		X int
+//		Y int
+//	}
+//
+//	func main() {
+//		v := Vertex{1, 2}
+//		defer fmt.Println(v)
+//		p := &v
+//		defer fmt.Println(p)
+//		p.X = 1e9
+//		defer fmt.Println(v, *p)
+//	}
+type list struct {
+	name1, name2, name3, name4, name5, name6, name7, name8, name9, name10 string
 }
 
 func main() {
-	v := Vertex{1, 2}
-	defer fmt.Println(v)
-	p := &v
-	defer fmt.Println(p)
-	p.X = 1e9
-	defer fmt.Println(v, *p)
+	m := make(map[string]list)
+	m["names"] = list{name1: "Alik", name2: "Baha", name3: "Vitya", name4: "Dilshod", name5: "Dima", name6: "Fatima", name7: "Ilya", name8: "Ivan", name9: "Svet", name10: "Vasya"}
+
+	var n string
+	fmt.Println("your name?")
+	fmt.Scanln(&n)
+
+	if _, ok := m[n]; ok {
+		fmt.Println(n, "is in the list")
+	} else {
+		fmt.Println(n, "is not in the list")
+	}
 }
