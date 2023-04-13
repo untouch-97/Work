b5b6d2420631d87fe5e030d3bc3bdcb90050c4f5
untouch <Bahodir5563325@gmail.com>
2023-04-13 22:30:10 +0500

    add test.go

--- test.go
+++ test.go
@@ -1,35 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
-//	type Vertex struct {
-//		X int
-//		Y int
-//	}
-//
-//	func main() {
-//		v := Vertex{1, 2}
-//		defer fmt.Println(v)
-//		p := &v
-//		defer fmt.Println(p)
-//		p.X = 1e9
-//		defer fmt.Println(v, *p)
-//	}
-type list struct {
-	name1, name2, name3, name4, name5, name6, name7, name8, name9, name10 string
+type struc struct {
+	name string
+	age  int
 }
 
 func main() {
-	m := make(map[string]list)
-	m["names"] = list{name1: "Alik", name2: "Baha", name3: "Vitya", name4: "Dilshod", name5: "Dima", name6: "Fatima", name7: "Ilya", name8: "Ivan", name9: "Svet", name10: "Vasya"}
+	maps := make(map[int]int)
+	maps[12] = 12
+	fmt.Printf(
+		"| map  %d, map %v,|\n",
+		reflect.ValueOf(maps).Pointer(),
+		*(*reflect.SliceHeader)(unsafe.Pointer(&maps)),
+	)
+	var slice []string
+	fmt.Printf(
+		"| slice  %d, slice %v,|\n",
+		reflect.ValueOf(slice).Pointer(),
+		*(*reflect.SliceHeader)(unsafe.Pointer(&slice)),
+	)
+	//var struc struc
+	//fmt.Printf(
+	//	"| map  %d, map %v,|\n",
+	//	reflect.ValueOf(struc).Pointer(),
+	//	*(*reflect.SliceHeader)(unsafe.Pointer(&struc)),
+	//)
 
-	var n string
-	fmt.Println("your name?")
-	fmt.Scanln(&n)
-
-	if _, ok := m[n]; ok {
-		fmt.Println(n, "is in the list")
-	} else {
-		fmt.Println(n, "is not in the list")
-	}
 }
